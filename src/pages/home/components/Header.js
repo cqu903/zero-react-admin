035aa7ee034c4ec171e1d5f0b7b7cdf796ad1dda
dave.zhao <zhaoxunyong@163.com>
2019-03-20 18:52:15 +0800

    Commit by new branch:dave.demo.x.

--- src/pages/home/components/Header.js
+++ src/pages/home/components/Header.js
@@ -1,28 +1,36 @@
 import React, { Component, Fragment } from 'react'
 import { Link } from 'react-router-dom'
-import { Row, Col, Badge } from 'antd'
+import { Row, Col, Badge, Icon } from 'antd'
 import Breadcrumb from './Breadcrumb'
 import routes from '../../routes'
+import styles from './sider.scss'
 
 class Header extends Component {
-    render() {
-        return (
-            <Fragment>
-                <Row>
-                    <Col span={6}>
-                        <Breadcrumb routes={routes} />
-                    </Col>
-                    <Col span={15}></Col>
-                    <Col span={3}>
-                        <Badge count={5} offset={[10, 0]}>
-                            <Link to='/wait_process_list'>
-                                <span style={{ fontSize: '14px' }}>待办事项</span>
-                            </Link>
-                        </Badge>
-                    </Col>
-                </Row>
-            </Fragment>
-        )
-    }
+  render() {
+    return (
+      <Fragment>
+        <Row>
+          <Col span={1}>
+            <Icon
+              className={styles.trigger}
+              type={this.props.collapsed ? 'menu-unfold' : 'menu-fold'}
+              onClick={() => this.props.onClick()}
+            />
+          </Col>
+          <Col span={6}>
+            <Breadcrumb routes={routes} />
+          </Col>
+          <Col span={14} />
+          <Col span={3}>
+            <Badge count={5} offset={[10, 0]}>
+              <Link to="/wait_process_list">
+                <span style={{ fontSize: '14px' }}>待办事项</span>
+              </Link>
+            </Badge>
+          </Col>
+        </Row>
+      </Fragment>
+    )
+  }
 }
 export default Header
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,47 +1,69 @@
 import React, { Component } from 'react'
-import { Layout } from 'antd';
+import { Layout } from 'antd'
 import Sider from './components/Sider'
 import { Route } from 'react-router-dom'
 import routes from '../routes'
 import Header from './components/Header'
 
 class Home extends Component {
-    render() {
-        console.info('home')
-        return (
-            <div id='components-layout-demo-custom-trigger'>
-                <Layout>
-                    <Layout.Sider
-                        collapsible={false}
-                        width={240}
-                        style={{ minHeight: 1500 }}
-                    >
-                        <Sider dataUrl={this.props.dataUrl} />
-                    </Layout.Sider>
-                    <Layout>
-                        <Layout.Header style={{ background: '#fff', padding: 0 }}>
-                            <Header />
-                        </Layout.Header>
-                        <Layout.Content style={{
-                            margin: '24px 16px',
-                            padding: 24,
-                            background: '#fff',
-                            minHeight: 280
-                        }}>
-
-                            {
-                                routes.map((route) => {
-                                    return (
-                                        <Route key={route.path} path={route.path} component={route.component} exact></Route>
-                                    )
-                                })
-                            }
-                        </Layout.Content>
-                    </Layout>
-                </Layout >
-            </div>
-
-        )
+  constructor(props) {
+    super(props)
+    this.toggle = this.toggle.bind(this)
+    this.state = {
+      collapsed: false
     }
+  }
+
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed
+    })
+  }
+
+  render() {
+    console.info('home')
+    return (
+      <div id="components-layout-demo-custom-trigger">
+        <Layout>
+          <Layout.Sider
+            trigger={null}
+            collapsible
+            collapsed={this.state.collapsed}
+            width={240}
+            style={{ minHeight: 1500 }}
+          >
+            <Sider dataUrl={this.props.dataUrl} />
+          </Layout.Sider>
+          <Layout>
+            <Layout.Header style={{ background: '#fff', padding: 0 }}>
+              <Header collapsed={this.state.collapsed} onClick={this.toggle} />
+            </Layout.Header>
+            <Layout.Content
+              style={{
+                margin: '24px 16px',
+                padding: 24,
+                background: '#fff',
+                minHeight: 280
+              }}
+            >
+              {routes.map(route => {
+                return (
+                  <Route
+                    key={route.path}
+                    path={route.path}
+                    component={route.component}
+                    exact
+                  />
+                )
+              })}
+            </Layout.Content>
+            <Layout.Footer style={{ textAlign: 'center' }}>
+              Copyright © 2019 zerofinance.cn. All Right Reserved.
+            </Layout.Footer>
+          </Layout>
+        </Layout>
+      </div>
+    )
+  }
 }
 export default Home
